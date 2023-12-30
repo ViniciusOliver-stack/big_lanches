c50e41c89e95942a3d10a47cae5532c678d465f8
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2023-12-30 03:24:52 -0300

    :sparkles: feat: listBifeArtesanal

---
+++ app/utils/listBifeArtesanal.ts
@@ -0,0 +1,60 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const listBifeArtesanal = [
+  {
+    id: uuidv4(),
+    name: "Hambúrguer Artesanal",
+    price: "17.00",
+    ingredients:
+      "Pão, bife artesanal, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "X-Artesanal",
+    price: "19.00",
+    ingredients:
+      "Pão, bife artesanal, queijo, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "X-Salada Artesanal",
+    price: "20.00",
+    ingredients:
+      "Pão, bife artesanal, queijo, presunto, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "Egg Artesanal",
+    price: "19.00",
+    ingredients:
+      "Pão, bife artesanal, ovo, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "X-Egg Artesanal",
+    price: "23.00",
+    ingredients:
+      "Pão, bife artesanal, ovo, queijo alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "Bacon Artesanal",
+    price: "22.00",
+    ingredients:
+      "Pão, bife artesanal, bacon, queijo, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "X-Egg Artesanal",
+    price: "25.00",
+    ingredients:
+      "Pão, bife artesanal, bacon, ovo, queijo, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "X-Tudo Artesanal",
+    price: "29.00",
+    ingredients:
+      "Pão, bife artesanal, bacon, ovo, queijo, presunto, alface, tomate, batata palha e milho verde.",
+  },
+]
