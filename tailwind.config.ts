1beb212c99339aede22009ff0af480fcb2cdd696
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2023-12-30 01:03:17 -0300

    :sparkles: feat: color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,17 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "yellow-dark": "#ECBB2A",
+        "red-dark": "#9F0B0A",
+        "black-medium": "#1E1D1D",
       },
     },
   },
