530f3ded58b5dc2079decedef6e8cf5fc5f27e7d
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2023-12-30 03:24:12 -0300

    :sparkles: feat: listBifePicanha

---
+++ app/utils/listBifePicanha.ts
@@ -0,0 +1,67 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const listBifePicanha = [
+  {
+    id: uuidv4(),
+    name: "Hambúrguer de Picanha",
+    price: "16.00",
+    ingredients:
+      "Pão, bife de picanha, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "X-Picanha",
+    price: "18.00",
+    ingredients:
+      "Pão, bife de picanha, queijo, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "X-Salada Picanha",
+    price: "19.00",
+    ingredients:
+      "Pão, bife de picanha, queijo, presunto, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "Egg Picanha",
+    price: "18.00",
+    ingredients:
+      "Pão, bife de picanha, ovo, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "X-Egg Picanha",
+    price: "22.00",
+    ingredients:
+      "Pão, bife de picanha, ovo, queijo, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "X-Bacon Picanha",
+    price: "22.00",
+    ingredients:
+      "Pão, bife de picanha, bacon, queijo, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "Bacon Picanha",
+    price: "20.00",
+    ingredients:
+      "Pão, bife de picanha, bacon, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "X-Egg Bacon Picanha",
+    price: "25.00",
+    ingredients:
+      "Pão, bife de picanha, bacon, ovo, queijo, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "X-Tudo Picanha",
+    price: "28.00",
+    ingredients:
+      "Pão, bife de picanha, bacon, ovo, queijo, presunto, alface, tomate, batata palha e milho verde.",
+  },
+]
