2d4527496f165a7374b35cac959056c7b61163c3
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2023-12-30 03:24:42 -0300

    :sparkles: feat: listBifeComum

---
+++ app/utils/listBifeComum.ts
@@ -0,0 +1,66 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const listBifeComum = [
+  {
+    id: uuidv4(),
+    name: "Hambúrguer",
+    price: "12.00",
+    ingredients: "Pão, bife comum, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "X-Burguer",
+    price: "15.00",
+    ingredients:
+      "Pão, bife comum, queijo, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "X-Salada",
+    price: "12.00",
+    ingredients:
+      "Pão, bife comum, queijo, alface, presunto, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "Egg Burguer",
+    price: "16.00",
+    ingredients:
+      "Pão, bife comum, ovo, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "X-Egg Burguer",
+    price: "20.00",
+    ingredients:
+      "Pão, bife comum, ovo, queijo, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "X-Bacon Burguer",
+    price: "20.00",
+    ingredients:
+      "Pão, bife comum, bacon, queijo, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "Bacon Burguer",
+    price: "17.00",
+    ingredients:
+      "Pão, bife comum, bacaon, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "X-Egg Bacon Burguer",
+    price: "24.00",
+    ingredients:
+      "Pão, bife comum, bacon, ovo, queijo, alface, tomate, batata palha e milho verde.",
+  },
+  {
+    id: uuidv4(),
+    name: "X-Tudo",
+    price: "26.00",
+    ingredients:
+      "Pão, bife comum, bacon, ovo, queijo, presunto, alface, tomate, batata palha e milho verde.",
+  },
+]
