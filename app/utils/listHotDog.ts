12e5a9f413417acfaa2fe473a1fee3e7d48d2c25
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2024-01-03 20:53:10 -0300

    feat: converting from string to number

--- app/utils/listHotDog.ts
+++ app/utils/listHotDog.ts
@@ -4,25 +4,25 @@ export const listHotDog = [
   {
     id: uuidv4(),
     name: "Cachorro-quente",
-    price: "11.00",
+    price: 11.0,
     ingredients: "Pão, 2 salsichas, batata palha e milho verde.",
   },
   {
     id: uuidv4(),
     name: "Cachorro-quente Premium",
-    price: "16.00",
+    price: 16.0,
     ingredients: "Pão, 2 salsichas, bacon, queijo, batata palha e milho verde.",
   },
   {
     id: uuidv4(),
     name: "Misteg",
-    price: "12.00",
+    price: 12.0,
     ingredients: "Pão de hambúrguer, ovo, presunto, queijo",
   },
   {
     id: uuidv4(),
     name: "Misto Quente",
-    price: "10.00",
+    price: 10.0,
     ingredients: "Pão de forma, presunto, queijo",
   },
 ]
