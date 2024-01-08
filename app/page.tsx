8ea7c89288c066aab93e29e34e9dc05b62dc7b8f
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2024-01-07 21:02:35 -0300

    :sparkles:	feat: language pt-BR

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,33 @@ import { Header } from "./components/Header"
 import { FilterFood } from "./components/FilterFood"
 import { Food } from "./components/Food"
 import Cart from "./cart/page"
-import { AppContextProvider } from "./hook/AppContext"
 
 export default function Home() {
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId)
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" })
+    }
+  }
+
+  const linkSections = [
+    {
+      name: "Lanches",
+      image: "https://i.imgur.com/GTgaZIH.png",
+      bgColor: "bg-red-dark",
+    },
+    {
+      name: "Hotdog",
+      image: "https://i.imgur.com/6hGQKaZ.png",
+      bgColor: "bg-yellow-500",
+    },
+    {
+      name: "Bebidas",
+      image: "https://i.imgur.com/YEwyruF.png",
+      bgColor: "bg-blue-500",
+    },
+  ]
+
   return (
     <div className="xl:grid xl:grid-cols-2 gap-9">
       <div>
@@ -15,9 +39,13 @@ export default function Home() {
 
         <section className="mt-16 px-8 pb-12">
           <ul className="flex items-center justify-around">
-            <FilterFood />
-            <FilterFood />
-            <FilterFood />
+            {linkSections.map((section) => (
+              <FilterFood
+                key={section.name}
+                section={section}
+                onClick={() => scrollToSection(`section${section.name}`)}
+              />
+            ))}
           </ul>
         </section>
         <Food />
