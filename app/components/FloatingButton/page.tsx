a270d86a6d4a0766b04acb8949ea6bac4fdb8534
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2024-01-27 01:14:39 -0300

    fixed actions to build

--- app/components/FloatingButton/page.tsx
+++ app/components/FloatingButton/page.tsx
@@ -1,11 +1,11 @@
+"use client";
+
 import { useAppContext } from "@/app/hook/AppContext";
 import Link from "next/link";
 import { FiShoppingCart } from "react-icons/fi";
 
 export default function FloatingButton() {
   const { cart } = useAppContext();
-
-  const hasItemsInCart = cart.length > 0;
 
   return (
     <Link
@@ -14,7 +14,7 @@ export default function FloatingButton() {
     >
       <FiShoppingCart />
 
-      {hasItemsInCart && (
+      {cart.length > 0 && (
         <span className="fixed bottom-14 right-4 bg-yellow-dark w-5 h-5 rounded-full ml-2 flex items-center justify-center text-xs">
           !
         </span>
