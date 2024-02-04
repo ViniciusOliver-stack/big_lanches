20d02ef140ba40397cc8a6e2938987eb3fc3d908
Vinicius Oliveira <vinicius.oliveira@melius.software>
2024-02-04 11:15:34 -0300

    feat: SEO optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,12 @@ export const metadata: Metadata = {
   title: "Big lanches - Trailer do Edelio",
   description:
     "Descubra os sabores irresistíveis do Big Lanches - Trailer do Edélio. Faça seu pedido online com facilidade e aproveite uma experiência gastronômica única. Peça agora!",
+  openGraph: {
+    images: [
+      "https://scontent-gru1-1.xx.fbcdn.net/v/t39.30808-6/305322855_465676388912502_1258770910836682795_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=783fdb&_nc_ohc=2HayzYsEQkEAX9yl03K&_nc_ht=scontent-gru1-1.xx&oh=00_AfBGCgvlBEzDnpenoneY3Hd8JtLHjfJZGiTR-Yj8C4_p0A&oe=65C48FC3",
+    ],
+    authors: ["Big Lanches - Trailer do Edelio"],
+  },
 }
 
 export default function RootLayout({
